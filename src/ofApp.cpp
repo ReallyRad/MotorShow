c87aab6e8fcca707159438225fc1883dea5a88ed
ReallyRad <arthur.pointeau@gmail.com>
2016-07-21 15:42:37 +0200

    changed IP to localhost

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 	//client.setMessageDelimiter("\TCP");
-	client.setup("192.168.1.33", 5555);
+	client.setup("127.0.0.1", 5555);
 	if (client.isConnected()) cout << "connected to server" << endl;
 	else cout << "connection failed" << endl;
 	started = false;
@@ -79,45 +79,46 @@ void ofApp::fitEdaData(ofxJSONElement e, float * d) {
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 
-		float hu = ofGetHeight()/10;
-		float wu = ofGetWidth()/13;
-		int radius = 5;
+	float hu = ofGetHeight() / 10;
+	float wu = ofGetWidth() / 13;
+	int radius = 5;
 
-		ofDrawBitmapString("alpha", wu, 2 * hu);
-		ofDrawBitmapString("beta", wu, 7 * hu);
-		ofDrawBitmapString("theta", 7 * wu, 2 * hu);
+	ofDrawBitmapString("alpha", wu, 2 * hu);
+	ofDrawBitmapString("beta", wu, 7 * hu);
+	ofDrawBitmapString("theta", 7 * wu, 2 * hu);
 
-		ofDrawBitmapString("EEG", 4 * wu, hu);
-		ofDrawBitmapString("EDA", 10 * wu, 6 * hu);
-		ofDrawBitmapString("ECG  ", 8 * wu, 9 * hu);
-				
-		ofDrawBitmapString("ch0", 2 * wu, 2 * hu);
-		ofDrawBitmapString("ch1", 2 * wu, 3 * hu);
-		ofDrawBitmapString("ch2", 2 * wu, 4 * hu);
-		ofDrawBitmapString("ch3", 2 * wu, 5 * hu);
+	ofDrawBitmapString("EEG", 4 * wu, hu);
+	ofDrawBitmapString("EDA", 10 * wu, 6 * hu);
+	ofDrawBitmapString("ECG  ", 8 * wu, 9 * hu);
 
-		ofDrawBitmapString("ch0", 2 * wu, 7 * hu);
-		ofDrawBitmapString("ch1", 2 * wu, 8 * hu);
-		ofDrawBitmapString("ch2", 2 * wu, 9 * hu);
-		ofDrawBitmapString("ch3", 2 * wu, 10 * hu);
+	ofDrawBitmapString("ch0", 2 * wu, 2 * hu);
+	ofDrawBitmapString("ch1", 2 * wu, 3 * hu);
+	ofDrawBitmapString("ch2", 2 * wu, 4 * hu);
+	ofDrawBitmapString("ch3", 2 * wu, 5 * hu);
 
-		ofDrawBitmapString("ch0", 8 * wu, 2 * hu);
-		ofDrawBitmapString("ch1", 8 * wu, 3 * hu);
-		ofDrawBitmapString("ch2", 8 * wu, 4 * hu);
-		ofDrawBitmapString("ch3", 8 * wu, 5 * hu);
+	ofDrawBitmapString("ch0", 2 * wu, 7 * hu);
+	ofDrawBitmapString("ch1", 2 * wu, 8 * hu);
+	ofDrawBitmapString("ch2", 2 * wu, 9 * hu);
+	ofDrawBitmapString("ch3", 2 * wu, 10 * hu);
 
-		ofDrawBitmapString("fasic", 8 * wu, 7 * hu);
-		ofDrawBitmapString("tonic", 8 * wu, 8 * hu);		
+	ofDrawBitmapString("ch0", 8 * wu, 2 * hu);
+	ofDrawBitmapString("ch1", 8 * wu, 3 * hu);
+	ofDrawBitmapString("ch2", 8 * wu, 4 * hu);
+	ofDrawBitmapString("ch3", 8 * wu, 5 * hu);
 
-		ofPushMatrix();		
-		
+	ofDrawBitmapString("fasic", 8 * wu, 7 * hu);
+	ofDrawBitmapString("tonic", 8 * wu, 8 * hu);
+
+	if (client.isConnected()) {
+		ofPushMatrix();
+
 		//draw alpha
 		for (int i = 0; i < 4; i++) {
 			vector<ofPoint> points;
-			for (int j = 0; j < 8; j++) {								
-				ofPoint point = ofPoint(3*wu + j*wu/2, (i + 2)*hu - eegAlpha[j][i] * hu *0.7);
+			for (int j = 0; j < 8; j++) {
+				ofPoint point = ofPoint(3 * wu + j*wu / 2, (i + 2)*hu - eegAlpha[j][i] * hu *0.7);
 				points.push_back(point);
 				ofCircle(point, radius);
 			}
@@ -125,7 +126,7 @@ void ofApp::draw(){
 				try {
 					ofLine(points.at(i - 1), points.at(i));
 				}
-				catch (exception& e){int x = 0;}
+				catch (exception& e) { int x = 0; }
 			}
 		}
 
@@ -160,8 +161,8 @@ void ofApp::draw(){
 				catch (exception& e) { int x = 0; }
 			}
 		}
-
-		ofPopMatrix();
+	}
+	ofPopMatrix();
 }
 
 void ofApp::sendCommand(string command) {
